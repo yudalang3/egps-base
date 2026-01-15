4cea09a858057b327ef69133c82bb8519e1417da
yudalang3 <dalangyusci@gmail.com>
2026-01-15 18:49:41 +0800

    chore(build): 更新构建脚本中的项目路径和依赖配置
    - 移除 .DS_Store 文件的忽略规则
    - 将 Windows 64 位路径更新为 x64 格式
    - 添加多个新的 eGPS 模块依赖路径
    - 扩展构建脚本以支持更多组件的打包
    - 优化脚本目录获取逻辑的可移植性

--- build_jar_and_remove.sh
+++ build_jar_and_remove.sh
@@ -2,8 +2,13 @@
 # Get script directory (works regardless of where script is called from)
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
-"${SCRIPT_DIR}/build_jar.sh" /mnt/c/Users/yudal/Documents/project/eGPS2/eGPS_v2.1_windows_64bit/dependency-egps/ \
-               /mnt/c/Users/yudal/Documents/project/eGPS2/eGPS_v2.1_windows_64bit_selfTest/dependency-egps/ \
+"${SCRIPT_DIR}/build_jar.sh" /mnt/c/Users/yudal/Documents/project/eGPS2/eGPS_v2.1_windows_x64/dependency-egps/ \
+               /mnt/c/Users/yudal/Documents/project/eGPS2/eGPS_v2.1_windows_x64_selfTest/dependency-egps/ \
                /mnt/c/Users/yudal/Documents/project/eGPS2/jars/egps2_collections/egps-main.gui/dependency-egps/ \
                /mnt/c/Users/yudal/Documents/project/eGPS2/jars/egps2_collections/egps-shell/dependency-egps/ \
-               /mnt/c/Users/yudal/Documents/project/eGPS2/jars/egps2_collections/egps-pathway.evol.browser/dependency-egps/
+               /mnt/c/Users/yudal/Documents/project/eGPS2/jars/egps2_collections/egps-pathway.evol.browser/dependency-egps/ \
+               /mnt/c/Users/yudal/Documents/project/eGPS2/jars/egps2_collections/egps-TQTools/dependency-egps/ \
+               /mnt/c/Users/yudal/Documents/project/eGPS2/jars/egps2_collections/egps-mutationPre/dependency-egps/ \
+               /mnt/c/Users/yudal/Documents/project/eGPS2/jars/egps2_collections/egps-sanky-venn/dependency-egps/ \
+               /mnt/c/Users/yudal/Documents/project/eGPS2/jars/egps2_collections/egps-heatmap/dependency-egps/ \
+               /mnt/c/Users/yudal/Documents/project/eGPS2/jars/egps2_collections/egps-chorddiagram/dependency-egps/
